97eb5ed7ef93855b6361ea45b5ba7ef6c3561fb4
marcus villanueva <marcusvillanueva@marcuss-MacBook-Neo.local>
2026-09-24 06:52:25 +0800

    Initial SpotReport prototype

---
+++ src/main/java/com/spotreport/spotreport/SpotreportApplication.java
@@ -0,0 +1,13 @@
+package com.spotreport.spotreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotreportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotreportApplication.class, args);
+	}
+
+}
