ec11c3cc9deb336729a26307eb0b00e940b9bca0
marcus villanueva <marcusvillanueva@marcuss-MacBook-Neo.local>
2026-09-24 08:17:22 +0800

    add user and admin func

---
+++ src/main/java/com/spotreport/spotreport/PageController.java
@@ -0,0 +1,23 @@
+package com.spotreport.spotreport;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+}
